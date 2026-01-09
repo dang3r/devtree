e71428465d12719b4067f301f534e0d3ece02072
Daniel Cardoza <dan@danielcardoza.com>
2026-01-08 19:09:53 -0500

    s

--- docs/_next/static/a5aInteVO_t-W5RIQjy1h/_buildManifest.js
+++ docs/_next/static/a5aInteVO_t-W5RIQjy1h/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/devtree//_next/:path+",
+        "destination": "/devtree/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
